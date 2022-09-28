93cb48c3dedae56c5b6b872980adaaa41b2af6c1
Aphenphos <MaxwillWinters@gmail.com>
2022-09-28 15:42:41 -0400

    added supabase auth

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+function getUser() {
+    return client.auth.currentUser;
+}
+
+async function authUser(email, password, type) {
+    let response;
+    if (type === 'sign-up') {
+        response = await client.auth.signUp({ email, password });
+    } else {
+        response = await client.auth.signIn({ email, password });
+    }
+
+    return response.user;
+} 
+
+async function signOut() {
+    await client.auth.signOut();
+}
+
+export { getUser, authUser, signOut } 
